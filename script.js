4219ca54d25982b2b6e2b13e87261cf3da67070f
Mariia Paraketsova <paraketsova@gmail.com>
2020-09-10 22:25:29 +0200

    Add throw the dice, add checkFullHouse

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+    let button = document.getElementById('throw_of_dice');
+    button.addEventListener('click', handleDiceButton);
+});
+
+let handleDiceButton = function() {
+    let dice = throwTheDice();
+    // fillNumberCells(dice);
+    // fillSumsAndBonus(dice);
+    fillSpecialCombinations(dice);
+    // fillTotal();
+};
+
+let throwTheDice = function(event) {
+    let diceResult = [];
+    for (let i=1; i<=5; i++) {
+        let position = getRndInteger(1, 6);
+        diceResult.push(position);
+    }
+    console.log(diceResult);
+    return countDice(diceResult);
+};
+
+let getRndInteger = function(min, max) {
+    return Math.floor (Math.random() * (max - min + 1) ) + min;
+};
+
+let countDice = function(dice) {
+    let values = [];
+    
+    for (let i = 0; i <= 5; i++) {
+        values[i] = 0;
+    }
+    for (let current_dice of dice) {
+          values[current_dice-1]++;
+    }
+
+    console.log(values);
+
+    return values;
+};
+
+// TODO: rewtrite this as fillNumberCells()
+/*
 let calculateSums = function(event) {
     for (let i=1; i<=4; i++) {
         let sum = 0;
@@ -14,31 +58,31 @@ let calculateSums = function(event) {
         }
     };
 };
+ */
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    let button = document.getElementById('calc');
-    button.addEventListener('click', calculateSums);
-});
+let fillSpecialCombinations = function(dice) {
+    if (checkFullHouse(dice)) {
+        document.getElementById('player1_fullhouse').value = 1;
+    }
+    else {
+        document.getElementById('player1_fullhouse').value = 0;
+    }
+};
 
-/*   
-function countDice(dice) {
-  let values = [];
-  
-  for (let i = 0; i <= 6; i++) {
-  	values[i] = 0;
-  }
-    // [0, 0, 0, 0, 0, 0, 0]
-  // [0, 0, 0, 1, 0, 0, 0]
-  // [0, 0, 0, 1, 1, 0, 0]
-  // [0, 0, 0, 1, 1, 1, 0]
-  // [0, 0, 0, 1, 1, 2, 0]
-  // [0, 0, 0, 1, 1, 2, 1]
-  for (let current_dice of dice) {
-		values[current_dice]++;
-  }
-  
-  console.log(values);
-}
-
-countDice([3, 4, 5, 5, 6]);
-*/
+let checkFullHouse = function(dice) {
+    // TODO: algorithm for the check
+    let values = [];
+    let pair = false;
+    let threeOfKind = false;
+    for (let i = 0; i < dice.length; i++) {
+       if (dice[i] === 2) {
+           pair = true;
+       }
+       else if (dice[i] === 3) {
+           threeOfKind = true;
+       }
+    };
+    if (pair && threeOfKind) {
+        return true;
+    }
+};
